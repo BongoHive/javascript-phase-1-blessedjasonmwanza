5e49f7f371c9458e85cd92b97568a17d96c469de
Blessed Jason Mwanza <mwanzabj@gmail.com>
2019-06-10 11:31:05 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,12 @@
 // you solution should be here
+let birthYear = 1998;
+let futureYear = 2025;
+let futureAge = futureYear - birthYear;
+console.log(`by the year ${futureYear}, i'll be ${futureAge} years old \n\n`);
+
+let currentAge = new Date().getFullYear() - birthYear;
+let numOfDaysPerYear = 365;
+let maxAge = 120;
+let perDayUse = 2;
+console.log(` Based on my daily intake of ${perDayUse} fruits, i will need ${(perDayUse * numOfDaysPerYear * maxAge) } fruits to last me until my ripe old age of ${maxAge} Year .`);
+    
